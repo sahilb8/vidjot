c93ecf6ca948f1738d5ab6a3c9d296aecf1e39dc
sahil bhanastarkar <sahilbanastarkar7184@gmail.com>
2019-12-05 18:56:05 +0530

    add templates and bootsrap theme

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 const express =require("express");
+const exphbs  = require('express-handlebars');
 
 const app = express();
 
+
+//how middleware works
+// app.use(function(req,res,next){
+//     console.log(Date.now());
+//     req.name="Sahil";
+//     next();
+// });
+
+
+//HANDLEBARS MIDDLEWARE
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
 //INDEX ROUTE
 app.get("/",(req,res)=>{
-    res.send("<h1>Hello World</h1>");
+    //you can pass this variable title to the insex.handlebars and use it there
+    const title="Welcome";
+    res.render("index",{
+        title: title
+    });
+    // console.log(req.name);
 });
 
 //ABOUT ROUTE
 app.get("/about",(req,res)=>{
-    res.send("This is the about page");
+    res.render("about");
 });
 const port = 5000;
 //the ()=> indicates callback function, same as function()
