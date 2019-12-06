4e6c25b14c81973ad73143df470f710c40bea61e
sahil bhanastarkar <sahilbanastarkar7184@gmail.com>
2019-12-06 20:55:08 +0530

    adding data to mongoose

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-const express =require("express");
+const express = require("express");
 const exphbs  = require('express-handlebars');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 
 const app = express();
+//map global promise - get rid of warning
+mongoose.Promise=global.Promise;
+//connect with MongoDB
+mongoose.connect("mongodb://localhost/vidjotdb",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(()=> console.log("MongoDB connected"))
+.catch(err=> console.log(err));
+
+//Load Idea model
+require('./models/Idea');
+const Idea = mongoose.model("ideas");
 
 
 //how middleware works
@@ -16,6 +31,10 @@ const app = express();
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+//BODY-PARSER middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 //INDEX ROUTE
 app.get("/",(req,res)=>{
     //you can pass this variable title to the insex.handlebars and use it there
@@ -26,6 +45,41 @@ app.get("/",(req,res)=>{
     // console.log(req.name);
 });
 
+//add Idea Form route
+app.get("/ideas/add",(req,res)=>{
+    res.render("ideas/add");
+});
+
+//handle idea post request
+app.post("/ideas",(req,res)=>{
+    let errors = [];
+    if(!req.body.title){
+        errors.push({text:"Please add a title"});
+    }
+    if(!req.body.details){
+        errors.push({text:"Please add details"});
+    }
+
+    if(errors.length > 0){
+        res.render("ideas/add",{
+            errors: errors,
+            title: req.body.title,
+            details: req.body.details
+        });
+    }else{
+        //This code is when you have no errors
+        const NewUser = {
+            title : req.body.title,
+            details: req.body.details
+        }
+        new Idea(NewUser)
+            .save()
+            //this is the promise that will return once its saved on the database.
+            .then(idea =>{
+                res.redirect("/ideas");
+            })
+    }
+});
 //ABOUT ROUTE
 app.get("/about",(req,res)=>{
     res.render("about");
---
+++ models/Idea.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const IdeaSchema = new Schema({
+    title:{
+        type: String,
+        required:true
+    },
+    details:{
+        type: String,
+        required:true,
+    },
+    date:{
+        type: Date,
+        default:Date.now
+    }
+});
+
+//we are creating the model here.
+//we will call this schema ideas and it will be connected to IdeaSchema
+mongoose.model('ideas',IdeaSchema);
