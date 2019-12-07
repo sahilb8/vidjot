a489071037609dbc424b24bba3f0a809f49bf011
sahil bhanastarkar <sahilbanastarkar7184@gmail.com>
2019-12-07 22:03:38 +0530

    manipulating ideas functionality

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const exphbs  = require('express-handlebars');
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
 
@@ -31,9 +34,30 @@ const Idea = mongoose.model("ideas");
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+//METHOD-OVERRIDE MIDDLEWARE
+app.use(methodOverride('_method'))
+
 //BODY-PARSER middleware
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+//EXPRESS-SESSION MIDDLEWARE
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  }));
+
+//CONNECT-FLASH MIDDLEWARE
+app.use(flash());
+
+//GLOBAL VARIABLE
+app.use(function(req,res,next){
+    res.locals.success_msg = req.flash("success_msg");
+    res.locals.error_msg = req.flash("error_msg");
+    res.locals.error = req.flash("error");
+    next();
+});
 
 //INDEX ROUTE
 app.get("/",(req,res)=>{
@@ -45,9 +69,38 @@ app.get("/",(req,res)=>{
     // console.log(req.name);
 });
 
+//ABOUT ROUTE
+app.get("/about",(req,res)=>{
+    res.render("about");
+});
+
+//IDEA index route
+app.get("/ideas",(req,res)=>{
+    Idea.find({})
+    .sort({date:'desc'})
+    .then(ideas=>{
+        res.render("ideas/index",{
+            ideas:ideas
+        });
+    });
+});
+
 //add Idea Form route
 app.get("/ideas/add",(req,res)=>{
     res.render("ideas/add");
+});
+
+//edit Idea Form route, over here in url :id represents the idea id which we want to edit,we can access it using req.params.id
+app.get("/ideas/edit/:id",(req,res)=>{
+    Idea.findOne({
+        _id: req.params.id
+    })
+    .then(idea =>{
+        res.render("ideas/edit",{
+            idea:idea
+        });
+    });
+   
 });
 
 //handle idea post request
@@ -76,14 +129,38 @@ app.post("/ideas",(req,res)=>{
             .save()
             //this is the promise that will return once its saved on the database.
             .then(idea =>{
+                req.flash("success_msg","App Idea Inserted");
                 res.redirect("/ideas");
             })
     }
 });
-//ABOUT ROUTE
-app.get("/about",(req,res)=>{
-    res.render("about");
+
+//handle idea edit put request
+app.put('/ideas/:id',(req,res)=>{
+    Idea.findOne({
+        _id: req.params.id
+    })
+    .then(idea =>{
+        idea.title= req.body.title;
+        idea.details= req.body.details;
+        idea.save()
+        .then(idea=>{
+            req.flash("success_msg","App Idea Updated");
+            res.redirect("/ideas");
+    });
+   
+        });
 });
+
+//delete an idea,  as long as the methods are different you can same url.
+app.delete("/ideas/:id",(req,res)=>{
+    Idea.remove({_id:req.params.id})
+    .then(()=>{
+        req.flash("success_msg", "App Idea Removed");
+        res.redirect("/ideas");
+    });
+});
+
 const port = 5000;
 //the ()=> indicates callback function, same as function()
 app.listen(port,()=> {
